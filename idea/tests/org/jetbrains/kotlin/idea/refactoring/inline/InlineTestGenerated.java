e9fbf957f3656325ac634cfb1039602b420bd9c9
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-05-30 19:57:11 +0300

    ++ inline

--- idea/tests/org/jetbrains/kotlin/idea/refactoring/inline/InlineTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/refactoring/inline/InlineTestGenerated.java
@@ -937,6 +937,12 @@ public class InlineTestGenerated extends AbstractInlineTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("DeeperNestedCall2.kt")
+            public void testDeeperNestedCall2() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("idea/testData/refactoring/inline/inlineVariableOrProperty/explicateTypeArgument/DeeperNestedCall2.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("EnoughDontExplicate.kt")
             public void testEnoughDontExplicate() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("idea/testData/refactoring/inline/inlineVariableOrProperty/explicateTypeArgument/EnoughDontExplicate.kt");
